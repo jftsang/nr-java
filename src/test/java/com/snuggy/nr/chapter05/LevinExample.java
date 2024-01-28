ada6a2791d4b5a7fbb3ce4ba8c21f2c4c72dd035
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2024-01-28 21:50:25 +0000

    Move, refactor some tests

--- src/test/java/com/snuggy/nr/chapter05/LevinExample.java
+++ src/test/java/com/snuggy/nr/chapter05/LevinExample.java
@@ -7,7 +7,7 @@ import com.snuggy.nr.chapter06.*;
 import com.snuggy.nr.util.*;
 import java.util.function.DoubleUnaryOperator;
 
-public class Levex {
+public class LevinExample {
 
     /**
      * Integrand for (5.3.22).
@@ -27,26 +27,21 @@ public class Levex {
         }
     }
 
-    public int main_levex() throws NRException {
+    public static void main(String[] args) throws NRException {
         // This sample program shows how to use the Levin u transformation to
         // evaluate an oscillatory integral, equation (5.3.22).
         final double PI = 3.141592653589793;
         int nterm = 12;
         double beta = 1.0, a = 0.0, b = 0.0, sum = 0.0;
         Levin series = new Levin(100, 0.0);
-        // cout << setw(5) << "N" << setw(19) << "Sum (direct)" << setw(21)
-        // << "Sum (Levin)" << endl;
         for (int n = 0; n <= nterm; n++) {
             b += PI;
             double s = qromb(new AFunc(), a, b, 1.e-8);
             a = b;
             sum += s;
             double omega = (beta + n) * s; // Use u transformation.
-            @SuppressWarnings("unused")
             double ans = series.next(sum, omega, beta);
-            // cout << setw(5) << n << fixed << setprecision(14) << setw(21)
-            // << sum << setw(21) << ans << endl;
+            System.out.println("N = " + n + ", Sum = " + sum + ", Levin Sum = " + ans);
         }
-        return 0;
     }
 }
